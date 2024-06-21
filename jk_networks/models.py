3ebc214a71b932bba801da4ab02106e29c7b5268
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2024-06-21 13:44:49 -0500

    LSTM aggregation for JK network

--- jk_networks/models.py
+++ jk_networks/models.py
@@ -4,8 +4,9 @@ from torch.nn import Linear
 from typing import List, Optional
 import torch.nn.functional as F
 import torch.nn.functional as F
+from torch.nn import LSTM
 class GCN_JK_Concat(torch.nn.Module):
-  def __init__(self, starting_features: int, hidden_channels: List[int], output_features: int, agg_method: str = 'concat'):
+  def __init__(self, starting_features: int, hidden_channels: List[int], output_features: int, agg_method: str = 'concat', lstm_hidden_channels: int = None):
     super().__init__()
     self.convs = torch.nn.ModuleList()
     self.convs.append(GCNConv(starting_features, hidden_channels[0]))
@@ -13,7 +14,6 @@ class GCN_JK_Concat(torch.nn.Module):
     for input_channels, output_channels in zip(hidden_channels[:-1], hidden_channels[1:]):
       self.convs.append(GCNConv(input_channels, output_channels))
 
-    
     if self.agg_method == 'concat':
       total_channels = sum(hidden_channels) + starting_features
       self.lin = Linear(total_channels, output_features)
@@ -22,8 +22,13 @@ class GCN_JK_Concat(torch.nn.Module):
         self.lin = Linear(hidden_channels[0], output_features)
       else:
         raise ValueError('Max pooling requires all hidden channels to be equal')
+    elif self.agg_method == 'lstm':
+      if all([channels_num == hidden_channels[0] for channels_num in hidden_channels]):
+        self.lin = Linear(hidden_channels[0], output_features)
+      self.lstm = LSTM(hidden_channels[-1], lstm_hidden_channels, num_layers=1, bidirectional=True)
+      self.lin = Linear(lstm_hidden_channels * 2, output_features) # multiplyed by 2 because bidirectional
   def forward(self, x: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
-    layer_outputs = [x] if self.agg_method == 'concat' else []
+    layer_outputs = [x] if self.agg_method in ['concat'] else []
     h_i = x
     for conv in self.convs:
       h_i = conv(h_i, edge_index)
@@ -35,7 +40,11 @@ class GCN_JK_Concat(torch.nn.Module):
       agg = torch.cat(layer_outputs, dim=1)
     elif self.agg_method == 'max pool':
       agg = torch.stack(layer_outputs).max(dim=0)[0]
-
+    elif self.agg_method == 'lstm':
+      layer_outputs = torch.stack(layer_outputs, dim=1)
+      agg, _ = self.lstm(layer_outputs)
+      agg = agg[:, -1, :]
+      
     return self.lin(agg)
 
 
